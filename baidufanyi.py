95a6cce0022c55ed7696f11a346054e92fbd2915
thearmada <1622797605.com>
2018-06-04 20:42:39 +0800

    virtualenv

--- baidufanyi.py
+++ baidufanyi.py
@@ -9,8 +9,11 @@ headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
 }
 data = {
-	
+	'kw': word
 }
 
-request = urllib.request(url=url, headers=headers)
-response = 
+data = urllib.parse.urlencode(data).encode('utf-8')
+request = urllib.request.Request(url=url, headers=headers, data=data)
+response = urllib.request.urlopen(request)
+
+print(response.read().decode('utf-8'))
