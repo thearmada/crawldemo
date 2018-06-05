1a785cb25c6a5498b6c6c73c8adcc63ffea42190
thearmada <1622797605@qq.com>
2018-06-05 15:47:05 +0800

    baidufanyi

--- baidufanyi.py
+++ baidufanyi.py
@@ -1,13 +1,15 @@
 import urllib.request
+
 import urllib.parse
 
-url = {
-	'http://fanyi.baidu.com/sug'
-}
+url = 'http://fanyi.baidu.com/sug'
+
 
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
 }
+
+word = 'papa'
 data = {
 	'kw': word
 }
